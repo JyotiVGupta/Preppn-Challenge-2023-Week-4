7ba0834ed3c51d5db70109eab00b41569f7d864d
Jyoti <jyotigupta306@gmail.com>
2024-06-04 12:13:42 +0100

    first

--- SCRIPT.sql
+++ SCRIPT.sql
@@ -1,34 +1,7 @@
-# Preppn-Challenge-2023-Week-4
 
-Solving Tableau Prep challenge 2023 Week 4 using SQL/Snowflake
+** Solution 
 
-**TASK**
-
-2023 Week 4 - Data Source Bank acquires new customers every month. They are stored in separate tabs of an Excel workbook. We'd like to consolidate all the months into one dataset, to do comparisons between months.
-
-- Stack the tables on top of one another i.e. Union them all.
-
-- Make a Joining Date field based on the Joining Day, Table Names and the year 2023.
-
-- Reshape the data so there is a field for each demographic, for each new customer.
-
-- Remove duplicates.
-
-**SQL/Snowflake Techniques Used**
-
-1.  Common Table Expressions
-2.  Pivot and column type-conversion
-3.  UNION ALL
-4.  Removing Duplicates
-
-## Solution
-
-### 1. Union the Tables
-
-As all the tables had same fields, I used UNION ALL to union the tables. I created an extra column called 'table name' and record the month number the table belonged to in this column. I stored this new table in
-
-```
-            WITH C as
+    WITH C as
 
                     (SELECT *, '01' as table_name
                                     FROM PD2023_WK04_JANUARY
@@ -72,15 +45,8 @@ As all the tables had same fields, I used UNION ALL to union the tables. I creat
                             UNION ALL
                     SELECT *, '12' as table_name
                                     FROM PD2023_WK04_DECEMBER
+                    ),
 
-)
-```
-
-### 2. Creating Joining date and Reshaping the data
-
-Used Date_Part to create th date from the three columns (2023, Table name and Joining Day). Then Pivoted the data using CTE and PIVOT function
-
-```
             Pre As (
                     SELECT   ID,
                             date_from_parts(2023, DATE_PART('month', Date(table_name, 'Auto')), Joining_Day) as Joining_date,
@@ -100,23 +66,12 @@ Used Date_Part to create th date from the three columns (2023, Table name and Jo
                                         PIVOT (MAX(Value) FOR Demographic IN ('Ethnicity', 'Account Type', 'Date of Birth')) As P
                                                     (ID, Joining_date, ethnicity,account_type, date_of_birth)
 )
-```
 
-### 3. Removing the duplicates
-
-In the Post CTE, I created a column 'rn' which gives a row number for each row restarting at every ID. That means for duplicate IDs row number will be 1, 2,.. and so on while for Distinct IDs it will be just 1. To remove duplicate rows, simply keep rows with rn=1
-
-```
-                     SELECT ID,
+   
+                    SELECT ID,
                             Joining_date,
-                            ethnicity,
+                            ethnicity, 
                             account_type,
                             date_of_birth
                                     FROM POST
                                         where rn = 1 ;
-
-```
-
-The output:
-
-<img src= 'Output/Output.png' width = '600'>
